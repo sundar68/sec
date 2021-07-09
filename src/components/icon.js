21e12829e83b0bff594147866bccf3743cc89523
sundar68 <80591471+sundar68@users.noreply.github.com>
2021-07-09 05:54:04 +0530

    Update icon.js

--- src/components/icon.js
+++ src/components/icon.js
@@ -12,24 +12,24 @@ export default function SocialFollow() {
   return (
     <div class="social">
       <div class="btnsame btn1">
-      <a href="https://www.facebook.com/enviropromise/"
+      <a href="https://www.facebook.com/smily.sundar.73"
           className="facebook social">
            <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
       </div>
       <div class="btnsame btn2">
-      <a href="https://www.linkedin.com/company/enviropromise"
+      <a href="https://www.linkedin.com/in/keerthi-satya-sai-sundar-900a0b196"
           className="linkedin social">
          <FontAwesomeIcon icon={faLinkedin} size="2x" />
       </a>
       </div>
       <div class="btnsame btn3">
-      <a href="https://twitter.com/enviro_promise?s=21" className="twitter social">
+      <a href="https://github.com/sundar68" className="twitter social">
            <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
       </div>
       <div class="btnsame btn4">
-      <a href="https://instagram.com/enviro_promise?utm_medium=copy_link" className="instagram social">
+      <a href="https://instagram.com/radnus__248?r=nametag" className="instagram social">
          <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a> 
       </div>
